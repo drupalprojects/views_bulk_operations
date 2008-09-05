2268837d92e59db5152118c3deb62b0b4a2568c0
Karim Ratib <infojunkie@48424.no-reply.drupal.org>
2008-09-05 11:05:18 +0000

    Better UI logic in Select All feature

--- views_bulk_operations.js
+++ views_bulk_operations.js
@@ -3,27 +3,39 @@
 Drupal.vboSelectAll = function() {
   var table = this;
 
-  thSelectAll = $("th.select-all", table).click(function() {
-    if ($("input.form-checkbox", thSelectAll[0])[0].checked) {
-      tdSelectAll = $("td.view-field-select-all").css("display", "table-cell");
-      $("input#vbo-select-all-pages", tdSelectAll).click(function() {
-        $("span#vbo-this-page", tdSelectAll).css("display", "none");
-        $("span#vbo-all-pages", tdSelectAll).css("display", "inline");
-        $("form input#edit-nodes-select-all").attr("value", 1);
-      });
-      $("input#vbo-select-this-page", tdSelectAll).click(function() {
-        $("span#vbo-this-page", tdSelectAll).css("display", "inline");
-        $("span#vbo-all-pages", tdSelectAll).css("display", "none");
-        $("form input#edit-nodes-select-all").attr("value", 0);
-      });
+  thSelectAll = $('th.select-all', table).click(function() {
+    cbSelectAll = $('input.form-checkbox', thSelectAll)[0];
+    setSelectAll(false);
+    $('input#vbo-select-all-pages', tdSelectAll).click(function() {
+      setSelectAll(true);
+    });
+    $('input#vbo-select-this-page', tdSelectAll).click(function() {
+      setSelectAll(false);
+    });
+    $('td input:checkbox', table).click(function() {
+      setSelectAll($('form input#edit-nodes-select-all').attr('value') == 1);
+    });
+  });
+
+  function setSelectAll(all) {
+    if (cbSelectAll.checked) {
+      tdSelectAll = $('td.view-field-select-all').css('display', 'table-cell');
+      if (all) {
+        $('span#vbo-this-page', tdSelectAll).css('display', 'none');
+        $('span#vbo-all-pages', tdSelectAll).css('display', 'inline');
+        $('form input#edit-nodes-select-all').attr('value', 1);
+      }
+      else {
+        $('span#vbo-this-page', tdSelectAll).css('display', 'inline');
+        $('span#vbo-all-pages', tdSelectAll).css('display', 'none');
+        $('form input#edit-nodes-select-all').attr('value', 0);
+      }
     }
     else {
-      tdSelectAll = $("td.view-field-select-all").css("display", "none");
-      $("span#vbo-this-page", tdSelectAll).css("display", "inline");
-      $("span#vbo-all-pages", tdSelectAll).css("display", "none");
-      $("form input#edit-nodes-select-all").attr("value", 0);
+      tdSelectAll = $('td.view-field-select-all').css('display', 'none');
+      $('form input#edit-nodes-select-all').attr('value', all ? 1 : 0);
     }
-  });
+  }
 }
 
 if (Drupal.jsEnabled) {
