590c970fd89308c65ccc48c160c2498792d26a46
Marcin Grabias <mgrabias@ogtrading.eu>
2017-12-27 17:14:52 +0100

    Issue #2932174: Add keyboard support for persistent selection AJAX

--- js/frontUi.js
+++ js/frontUi.js
@@ -24,10 +24,44 @@
     display_id: '',
     list: {},
     $placeholder: null,
+
+    /**
+     * Bind event handlers to an element.
+     *
+     * @param {jQuery} element
+     */
+    bindEventHandlers: function ($element) {
+      var selectionObject = this;
+      $element.on('keypress', function (event) {
+        // Emulate click action for enter key.
+        if (event.which === 13) {
+          event.preventDefault();
+          event.stopPropagation();
+          selectionObject.update(this.checked, $(this).val());
+          $(this).trigger('click');
+        }
+        if (event.which === 32) {
+          selectionObject.update(this.checked, $(this).val());
+        }
+      });
+      $element.on('mousedown', function (event) {
+        // Act only on left button click.
+        if (event.which === 1) {
+          selectionObject.update(this.checked, $(this).val());
+        }
+      });
+    },
+
+    /**
+     * Perform an AJAX request to update selection.
+     *
+     * @param {bool} state
+     * @param {string} value
+     */
     update: function (state, value = null) {
       if (this.view_id.length && this.display_id.length) {
         var list = {};
-        if (value) {
+        if (value && value != 'on') {
           list[value] = this.list[value];
         }
         else {
@@ -73,18 +107,13 @@
       Drupal.viewsBulkOperationsSelection.list = {};
       $viewsTable.find('tbody .views-field-views-bulk-operations-bulk-form input[type="checkbox"]').each(function () {
         Drupal.viewsBulkOperationsSelection.list[$(this).val()] = $(this).parent().find('label').first().text();
-        $(this).on('mousedown', function (event) {
-          Drupal.viewsBulkOperationsSelection.update(this.checked, $(this).val());
-        });
+        Drupal.viewsBulkOperationsSelection.bindEventHandlers($(this));
       });
       Drupal.viewsBulkOperationsSelection.view_id = $multiSelectElement.attr('data-view-id');
       Drupal.viewsBulkOperationsSelection.display_id = $multiSelectElement.attr('data-display-id');
 
-      // Add event handler to select all checkbox.
-      $tableSelectAll.on('mousedown', function (event) {
-        Drupal.viewsBulkOperationsSelection.update(this.checked);
-      });
-
+      // Bind event handlers to select all checkbox.
+      Drupal.viewsBulkOperationsSelection.bindEventHandlers($tableSelectAll);
     }
 
     var strings = {
@@ -105,7 +134,7 @@
       $allSelector.show();
     }
 
-    $('input', $allSelector).click(function (event) {
+    $('input', $allSelector).on('click', function (event) {
       event.preventDefault();
       if ($primarySelectAll.is(':checked')) {
         $primarySelectAll.prop('checked', false);
@@ -119,7 +148,7 @@
       }
     });
 
-    $tableSelectAll.change(function (event) {
+    $tableSelectAll.on('change', function (event) {
       if (this.checked) {
         $allSelector.show();
       }
